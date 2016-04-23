fddc4383f8dc71c5c355fbc7dfc81ab56f7aeec2
Pritam Sarkar <001pritam2012@gmail.com>
2016-04-23 09:57:24 +0530

    Added files via upload

---
+++ scripts/HealPickupManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealPickupManager : MonoBehaviour {
+	public int x, y, z;
+	public int healPoint = 20;
+	PlayerHealth playerHealth;
+	public GameObject player;
+	
+	void Awake ()
+    {
+        playerHealth = player.GetComponent <PlayerHealth> ();
+    }
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("ok");
+		playerHealth.TakeDamage (-healPoint);
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (x, y, z) * Time.deltaTime);
+	}
+}
---
+++ scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+	private int last_score;
+
+    Text text;
+	Animator anim;
+
+    void Awake ()
+    {
+		anim = GetComponent<Animator>();
+        text = GetComponent <Text> ();
+        score = 0;
+		last_score = score;
+    }
+
+
+    void Update ()
+    {
+		if (last_score != score)
+		{
+			Debug.Log(score);
+			anim.SetTrigger("ScoreChanged");
+			last_score = score;
+        }
+		text.text = "" + score;
+    }
+}
